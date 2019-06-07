4b4d59688f899d6832fb81afd1b0c1b0dc586b6d
lincoln <lincolnllklee@163.com>
2019-06-07 21:56:12 +0800

    quick-commit

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-python run.py --model_type DemosaicSR --noisy 0.1 --debug True
-python run.py --model_type RYYB --noisy 0.1 --debug True
-python run.py --model_type Arbitrary --noisy 0.1 --debug True
+python run.py --model_type DemosaicSR --noisy 0.1
+python run.py --model_type RYYB --noisy 0.1
+python run.py --model_type Arbitrary --noisy 0.1
 
