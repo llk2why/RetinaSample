bced6f8a1e540a27be56ec1b9b10c658f16bbf46
lincoln <lincolnleellk@163.com>
2019-07-14 16:55:01 +0800

    [ADD] add npy file

--- calculate_energy.py
+++ calculate_energy.py
@@ -1,6 +1,20 @@
 import os
 import cv2
+import yaml
 import numpy as np
 
 from config import Dataset
 
+raw_dir = Dataset.RAW_DIR
+energy_dict = {}
+
+tifs = [x for x in os.listdir(raw_dir) if 'tif' in x.lower()]
+
+for tif in tifs:
+    fpath = os.path.join(raw_dir,tif)
+    img = cv2.imread(fpath)
+    energy = float(np.sqrt(np.mean(np.square(img))))
+    energy_dict[tif.split('.')[0]] = energy
+
+with open('yamls/energy.yaml','w') as f:
+    yaml.dump(energy_dict,f)
